cf832f43370ec545faac709c30afd06cb10d0fb7
CamilaQ <camila.quiros.07@gmail.com>
2023-05-12 01:50:10 -0300

    desafio completo

--- src/ProductManager.js
+++ src/ProductManager.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+import { promises } from 'fs';
 
 export default class ProductManager {
     products = [];
@@ -7,125 +7,26 @@ export default class ProductManager {
         this.path = path;
     }
 
-    cargaDelArchivo(){
-        const database = fs.readFileSync(this.path, "utf-8");
-        const databaseParsed = JSON.parse(database)
-        return databaseParsed;
-    }
-
-    getProducts() {
-        const database = this.cargaDelArchivo();
-        if(database.length === 0) return console.log("No hay productos")
-        else console.log(database)
-    }
-
-    addProduct(product) {
-        const database = this.cargaDelArchivo();
-        if (database.find((p) => p.code == product.code)){ 
-            console.log(`Producto ${product.title} ya existente`);
-        } else{
-            product.id = database.length + 1;
-            database.push(product);
-            fs.writeFileSync(this.path, JSON.stringify(database))
-        }
-    }
-
-    getProductsById(productId) {
-        const database = this.cargaDelArchivo();
-        const product = database.find((search) => search.id == productId)
-        if(product){
-            return console.log(product)
-        } else{
-            return console.log('Producto no encontrado')
-        }
-    }
-
-    updateProduct(productUpdate, {...productNew}){
-        const database = this.cargaDelArchivo();
-        this.deleteProduct(productUpdate);
-        const product = database.find((search) => search.id == productUpdate)
-        if(product){
-            this.deleteProduct(productUpdate);
-            const databaseNueva = this.cargaDelArchivo();
-            let productsModified = [{ ...productNew, id: productUpdate}, ...databaseNueva]
-            fs.writeFileSync(this.path, JSON.stringify(productsModified))
-        } else{
-            return console.log('Producto no encontrado')
-        }
-    }
-
-    deleteProduct(product){
-        const database = this.cargaDelArchivo();
-        if(database.find((search) => search.id == product)){
-            const databaseNueva = database.filter((search) => search.id !== product)
-            fs.writeFileSync(this.path, JSON.stringify(databaseNueva))
-        } else{
-            return console.log('Producto no encontrado')
+    async getProducts(){
+        try {
+            const database = await promises.readFile(this.path, "utf-8");
+            const databaseParsed = JSON.parse(database)
+            return databaseParsed;
+        } catch(error) {
+            console.log(error);
+            return [];
         }
     }
 }
 
 class Product extends ProductManager {
-    contructor(title, description, price, thumbnail, code, stock) {
+    contructor(title,category, price, description, image, stock) {
         this.id = id;
         this.title = title;
-        this.description = description;
+        this.category = category
         this.price = price;
-        this.thumbnail = thumbnail;
-        this.code = code;
+        this.description = description;
+        this.image = image;
         this.stock = stock;
     }
 }
-
-const nuevoProducto = new ProductManager('./database.json');
-
-const product1 = {
-    title: 'celular',
-    description: 'Lorem Ipsum',
-    price: 1000,
-    thumbnail: 'thumbnail',
-    code: 'abc123',
-    stock: 10
-}
-
-const product2 = {
-    title: 'computadora',
-    description: 'Lorem Ipsum',
-    price: 5000,
-    thumbnail: 'thumbnail',
-    code: 'abc124',
-    stock: 10
-}
-
-const product3 = {
-    title: 'tablet',
-    description: 'Lorem Ipsum',
-    price: 3000,
-    thumbnail: 'thumbnail',
-    code: 'abc125',
-    stock: 10
-}
-
-console.log(nuevoProducto.getProducts())
-
-nuevoProducto.addProduct(product1);
-nuevoProducto.addProduct(product2);
-nuevoProducto.addProduct(product3);
-
-console.log(nuevoProducto.getProducts())
-
-console.log(nuevoProducto.getProductsById(2))
-console.log(nuevoProducto.getProductsById(4))
-
-
-// console.log(nuevoProducto.deleteProduct(2))
-// console.log(nuevoProducto.deleteProduct(4))
-
-// console.log(nuevoProducto.updateProduct(1,  
-// {
-// title: 'celular',    
-// description: 'Lorem Ipsum',
-// price: 7000,
-// thumbnail: 'thumbnail',
-// code: 'abc124',
-// stock: 10}))
--- src/app.js
+++ src/app.js
@@ -1,13 +1,37 @@
 import express from "express";
-// import ProductManager from "../ProductManager";
+import ProductManager from "./ProductManager.js";
 
 const app = express();
 
-app.get('/products', (req, res) => {
-    res.send('texto')
+// Crear nueva instancia de la clase
+const productManager = new ProductManager('./src/products.json')
+
+app.use(express.urlencoded({extended: true}))
+
+//Mostrar un limite de productos desde el inicio(Ej: http://localhost:8080/products?limit=5) o Mostrar todos los productos si no se asigna un limite(Ej: http://localhost:8080/products)
+app.get('/products', async (req, res) => {
+    const products = await productManager.getProducts();
+    const {limit} = req.query;
+    if(limit != undefined){
+        const productLimited = products.slice(0,limit);
+        res.send(productLimited)
+    } else {
+        res.send(products)
+    }
 });
 
-app.listen(8080, () => console.log('Escuchando puerto 8080'));
+//Muestra un solo producto según id(Ej: http://localhost:8080/products/2), si no existe tira un status 404 y un mensaje de error(Ej: http://localhost:8080/products/26)
+app.get('/products/:pid', async (req, res) => {
+    const {pid} = req.params;
+    const products = await productManager.getProducts();
+    const product = products.find((product) => product.id == pid);
+    if(product) return res.send(product); 
+    else res.status(404).send('Producto no encontrado');
+})
+
+app.listen(8080, () => {
+    console.log('Escuchando puerto 8080')
+});
 
 
 
