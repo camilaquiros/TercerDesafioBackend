7e0081246da579e0606752f20bf170f6a17fb08e
CamilaQ <camila.quiros.07@gmail.com>
2023-05-11 22:44:21 -0300

    archivos ordenados

--- src/app.js
+++ src/app.js
@@ -1,3 +1,13 @@
-class ProductManager {
-    
-}
+import express from "express";
+// import ProductManager from "../ProductManager";
+
+const app = express();
+
+app.get('/products', (req, res) => {
+    res.send('texto')
+});
+
+app.listen(8080, () => console.log('Escuchando puerto 8080'));
+
+
+
